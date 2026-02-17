e8c2a10342ecd1cb1fec03e9f6129b1b70db52ed
Lucas Amoros <lamorosmenarguez@gmail.com>
2026-02-17 23:08:13 +0100

    reservas y panel de admin vistas hechas

---
+++ src/main/java/org/example/courtslot/controller/AdminPanelController.java
@@ -0,0 +1,6 @@
+package org.example.courtslot.controller;
+
+
+public class AdminPanelController {
+
+}
---
+++ src/main/java/org/example/courtslot/controller/MyBookingsController.java
@@ -0,0 +1,4 @@
+package org.example.courtslot.controller;
+
+public class MyBookingsController {
+}
